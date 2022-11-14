6a2b8fc169162e943a6770c1c937030b2f048201
AdiFischer <adifischer1@gmail.com>
2022-11-14 15:04:06 -0500

    bank done

--- program.cs
+++ program.cs
@@ -1,1 +1,15 @@
-Console.WriteLine("Hello world");
+Console.WriteLine("Welcome to the Bank");
+
+BankAccount account = new BankAccount(11000);
+
+//account.balance = 100;
+try{
+    account.makeDeposit(-1);
+} catch(Exception ex) {
+    Console.WriteLine(ex.Message);
+}
+
+//Console.WriteLine($"Your balance is: {account.balance} \n Account Number: {account.accountNumber}");
+Console.Write($"Your balance is: {account.getBalance()} ");
+Console.WriteLine($" Account Number: {account.accountNumber}");
+
